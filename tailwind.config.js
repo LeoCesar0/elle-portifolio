511ae5fde9d3f6e8862869b2111e348de3c15530
Leonardo César <leo_cesa@hotmail.com>
2024-04-20 12:48:07 -0300

    chore: config fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#fff",
+        onBackground: "#1B181A",
+        primary: "#EA607E",
+        onPrimary: "#FFFFFF",
+        muted: "rgba(27, 24, 26, 0.75)",
+        card: "#fff",
+        onCard: "rgba(45, 45, 45, 1)",
+      },
+    },
   },
   plugins: [],
 };
