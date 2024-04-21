1191c7802b55731d701dd865ec7f06e3aa2322af
Leonardo César <leo_cesa@hotmail.com>
2024-04-21 16:05:25 -0300

    feat: finish welcome section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -16,17 +16,33 @@ export const Welcome = () => {
     pt: "cria experiências digitais intuitivas e cativantes.",
   });
 
+  const tags = ["Figma", "Adobe", "React", "Javascript", "HTML", "CSS"];
+
   return (
-    <section className="mt-16  ">
-      <h1 className="font-semibold text-5xl leading-[72px] text-center mb-[24px]">
-        {hello}
-      </h1>
-      <p className="leading-6 text-center text-onBackground/75">
-        <strong className="font-semibold text-onBackground/75">
-          {descriptionStrong}
-        </strong>{" "}
-        {description}
-      </p>
+    <section className="mt-16 mb-[96px]">
+      <div className="mb-6">
+        <h1 className="font-semibold text-5xl leading-[72px] text-center mb-[24px]">
+          {hello}
+        </h1>
+        <p className="leading-6 text-center text-onBackground/75">
+          <strong className="font-semibold text-onBackground/75">
+            {descriptionStrong}
+          </strong>{" "}
+          {description}
+        </p>
+      </div>
+      <div className="carousel">
+        <ol className="carousel__viewport">
+          {tags.map((tag, index) => (
+            <li
+              key={index}
+              className="carousel__slide px-[20px] py-[10px] border border-border rounded-[30px]"
+            >
+              {tag}
+            </li>
+          ))}
+        </ol>
+      </div>
     </section>
   );
 };
