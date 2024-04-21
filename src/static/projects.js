4d656d376b516363c41d4eab6315457ec823a51a
Leonardo César <leo_cesa@hotmail.com>
2024-04-21 16:21:39 -0300

    feat: add projects

---
+++ src/static/projects.js
@@ -0,0 +1,86 @@
+export const PROJECTS = [
+  {
+    name: "Delivery",
+    imageTitle: "delivery-title.png",
+    image: "delivery-image.png",
+    description: {
+      en: "Delivery App created to assist with food orders and deliveries. To make that moment easier when you need to find a quick and tasty snack",
+      pt: "Aplicativo de Delivery criado para auxiliar com pedidos e entregas de comida. Para tornar mais fácil aquele momento em que você precisa encontrar um lanche rápido e saboroso",
+    },
+    tags: ["figma"],
+    buttons: [
+      {
+        buttonColor: "rgba(242, 78, 30, 1)",
+        buttonTextColor: "#fff",
+        link: "https://www.figma.com",
+        text: {
+          en: "Figma",
+          pt: "Figma",
+        },
+      },
+    ],
+  },
+  {
+    name: "FoodSafe",
+    imageTitle: "food-title.png",
+    image: "food-image.png",
+    description: {
+      en: "FoodSafe is a website to find fit recipes and nearby restaurants. With a broad community to share experience",
+      pt: "FoodSafe é um site para encontrar receitas saudáveis e restaurantes próximos. Com uma ampla comunidade para compartilhar experiências",
+    },
+    tags: ["figma"],
+    buttons: [
+      {
+        buttonColor: "rgba(116, 148, 21, 1)",
+        buttonTextColor: "#fff",
+        link: "https://www.figma.com",
+        text: {
+          en: "Figma",
+          pt: "Figma",
+        },
+      },
+    ],
+  },
+  {
+    name: "Lossi Landing Page",
+    imageTitle: "lossi-title.png",
+    image: "lossi-lp-image.png",
+    description: {
+      en: "Landing Page Lossi, is a women's cosmetics brand. Simple and easy to customize design",
+      pt: "Landing Page Lossi, é uma marca de cosméticos femininos. Design simples e fácil de personalizar",
+    },
+    tags: ["figma"],
+    buttons: [
+      {
+        buttonColor: "rgba(234, 96, 126, 1)",
+        buttonTextColor: "#fff",
+        link: "https://www.figma.com",
+        text: {
+          en: "Figma",
+          pt: "Figma",
+        },
+      },
+    ],
+  },
+  {
+    name: "Lossi eCommerce",
+    imageTitle: "lossi-title.png",
+    image: "lossi-e-image.png",
+    description: {
+      en: "eCommerce Lossi, is a women's cosmetics brand. Simple and easy to customize design",
+      pt: "eCommerce Lossi, é uma marca de cosméticos femininos. Design simples e fácil de personalizar",
+    },
+    tags: ["figma"],
+    buttons: [
+      {
+        buttonColor: "rgba(234, 96, 126, 1)",
+        buttonTextColor: "#fff",
+        link: "https://www.figma.com",
+        text: {
+          en: "Figma",
+          pt: "Figma",
+        },
+      },
+    ],
+  },
+];
