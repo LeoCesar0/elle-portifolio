336031b52c18113c17322b3f31bd0da4c9259e7e
Leonardo César <leo_cesa@hotmail.com>
2024-04-29 11:10:28 -0300

    fix: add both languages to concept section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
+import { useT } from "../hooks/useT";
+
 export const Card = ({ imagePath, title, description }) => {
+  const _title = useT(title);
+  const _description = useT(description);
+
   return (
     <div className="px-[24px] py-[38px] grid grid-cols-1 gap-[42px] card-shadow">
       <img className="mx-auto" src={imagePath} alt={title} />
       <div className="text-center">
         <h2 className="mb-[12px] text-center text-[27px] leading-[42px] font-semibold">
-          {title}
+          {_title}
         </h2>
-        <p className="text-center text-[17px] leading-[27px]">{description}</p>
+        <p className="text-center text-[17px] leading-[27px]">{_description}</p>
       </div>
     </div>
   );
--- src/components/ConceptSection.jsx
+++ src/components/ConceptSection.jsx
@@ -1,9 +1,55 @@
 import { Card } from "./Card";
 
 export const ConceptSection = () => {
+  const cards = [
+    {
+      imagePath: "images/responsive.svg",
+      title: {
+        en: "Responsive design",
+        pt: "Design responsivo",
+      },
+      description: {
+        en: "Make the site easier to view by displaying sites on different devices in ways that are easy to read and navigate",
+        pt: "Facilitar a visualização do site exibindo sites em diferentes dispositivos de maneiras fáceis de ler e navegar",
+      },
+    },
+    {
+      imagePath: "images/identity.svg",
+      title: {
+        en: "Visual identity",
+        pt: "Identidade visual",
+      },
+      description: {
+        en: "Creation of the visual identity of the landing page, working with UI UX concepts",
+        pt: "Criação da identidade visual da landing page, trabalhando com conceitos de UI UX",
+      },
+    },
+    {
+      imagePath: "images/designer-developer.svg",
+      title: {
+        en: "Designer + Developer",
+        pt: "Designer + Desenvolvedor",
+      },
+      description: {
+        en: "Organized and simple design to work alongside development",
+        pt: "Design organizado e simples para trabalhar junto ao desenvolvimento",
+      },
+    },
+  ];
+
   return (
     <section className="grid grid-cols-1 gap-[56px] px-[4px]">
-      <Card
+      {cards.map((card) => {
+        return (
+          <Card
+            key={card.imagePath}
+            imagePath={card.imagePath}
+            title={card.title}
+            description={card.description}
+          />
+        );
+      })}
+      {/* <Card
         imagePath="images/responsive.svg"
         title="Responsive design"
         description="Make the site easier to view by displaying sites on different devices in ways that are easy to read and navigate"
@@ -17,7 +63,7 @@ export const ConceptSection = () => {
         imagePath="images/designer-developer.svg"
         title="Design + Developer"
         description="Organized and simple design to work alongside development"
-      />
+      /> */}
     </section>
   );
 };
