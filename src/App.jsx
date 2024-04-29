9e571406d1652d5948dc0ad13066fc9343a13487
Leonardo César <leo_cesa@hotmail.com>
2024-04-29 11:20:00 -0300

    feat: add bottom section

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,21 @@ import { Header } from "./components/Header";
 import { Layout } from "./components/Layout";
 import { ProjectsSection } from "./components/ProjectsSection";
 import { Welcome } from "./components/Welcome";
+import { useT } from "./hooks/useT";
 
 function App() {
+  const letsTalkText = useT({
+    en: "Let's Talk!",
+    pt: "Entre em contato!",
+  });
+
+  const emailText = useT({
+    en: "send me an email",
+    pt: "me mande um email",
+  });
+
+  const email = "izaelleavrillavigne@gmail.com";
+
   return (
     <>
       <Layout>
@@ -15,6 +28,20 @@ function App() {
           <ProjectsSection />
           <ConceptSection />
           <AboutSection />
+          <div className="text-center mb-[96px]">
+            <p className="text-[32px] leading-[48px] mb-6">{letsTalkText}</p>
+            <button
+              className="text-[32px] leading-[48px] bg-primary text-onPrimary shadow-sm
+                px-[20px] py-[15px] flex items-center gap-[10px] rounded-[50px] mx-auto
+              "
+              onClick={() => {
+                window.location.href = "mailto:" + email;
+              }}
+            >
+              {emailText}
+              <img src="icons/send.svg" />
+            </button>
+          </div>
         </div>
       </Layout>
     </>
