7114d4ec1c922b80cd8b400e416a566504d1e4eb
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-05-06 17:53:20 -0300

    fix: remove testing css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 export const Layout = ({ children }) => {
   return (
-    <div className="py-[20px] px-[5px] mx-auto md:max-w-[1168px] border border-border md:px-[14px] md:py-[32px]">
+    <div className="py-[20px] px-[5px] mx-auto md:max-w-[1168px] md:px-[14px] md:py-[32px]">
       {children}
     </div>
   );
