981fb4207828f0a3faf0f8536cb12abe5d71e38f
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-05-06 18:04:13 -0300

    feat: make welcome section responsive

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -19,7 +19,7 @@ export const Welcome = () => {
   const tags = ["Figma", "Adobe", "React", "Javascript", "HTML", "CSS"];
 
   return (
-    <section className="mt-16 px-[4px]">
+    <section className="mt-16 px-[4px] mx-auto  max-w-[835px] w-full md:mb-[20px]">
       <div className="mb-6 md:max-w-[506px] md:mx-auto ">
         <h1 className="font-semibold text-5xl leading-[72px] text-center mb-[24px]">
           {hello}
@@ -32,7 +32,7 @@ export const Welcome = () => {
         </p>
       </div>
       <div className="carousel">
-        <ol className="carousel__viewport">
+        <ol className="carousel__viewport md:hidden">
           {tags.map((tag, index) => (
             <li
               key={index}
@@ -42,6 +42,27 @@ export const Welcome = () => {
             </li>
           ))}
         </ol>
+        <div className="hidden md:block">
+          <ol className="carousel__viewport items-center ">
+            {tags.slice(0, 2).map((tag, index) => (
+              <li
+                key={index}
+                className="carousel__slide px-[20px] py-[10px] border border-border rounded-[30px]"
+              >
+                {tag}
+              </li>
+            ))}
+            <div className="h-[1px] w-[81px] bg-border"></div>
+            {tags.slice(2).map((tag, index) => (
+              <li
+                key={index}
+                className="carousel__slide px-[20px] py-[10px] border border-border rounded-[30px]"
+              >
+                {tag}
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
     </section>
   );
