c5df10ac6964510cf50b403461057f731d15fcb3
Leonardo César <leo_cesa@hotmail.com>
2024-05-08 20:49:30 -0300

    feat: make concept section responsive

--- src/components/ConceptSection.jsx
+++ src/components/ConceptSection.jsx
@@ -38,7 +38,7 @@ export const ConceptSection = () => {
   ];
 
   return (
-    <section className="grid grid-cols-1 gap-[56px] px-[4px]">
+    <section className="grid grid-cols-1 gap-[56px] px-[4px] md:grid-cols-3 md:gap-[24px]">
       {cards.map((card) => {
         return (
           <Card
@@ -49,21 +49,6 @@ export const ConceptSection = () => {
           />
         );
       })}
-      {/* <Card
-        imagePath="images/responsive.svg"
-        title="Responsive design"
-        description="Make the site easier to view by displaying sites on different devices in ways that are easy to read and navigate"
-      />
-      <Card
-        imagePath="images/identity.svg"
-        title="Visual identity"
-        description="Creation of the visual identity of the landing page, working with UI UX concepts"
-      />
-      <Card
-        imagePath="images/designer-developer.svg"
-        title="Design + Developer"
-        description="Organized and simple design to work alongside development"
-      /> */}
     </section>
   );
 };
