d6fb7784daac24e30d6ae2f6722231cc5486fa89
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-07-14 20:12:50 -0300

    feat: add particles

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useT } from "../hooks/useT";
 
 export const AboutSection = () => {
@@ -7,22 +8,43 @@ export const AboutSection = () => {
   });
 
   const text1 = useT({
-    en: "Hello! 👋 My name is Izaelle Lavigne, and I am passionate about crafting digital experiences. Currently, I am studying web design and programming, delving into the realm of online creation.",
-    pt: "Olá! 👋 Meu nome é Izaelle Lavigne, e sou apaixonada por criar experiências digitais. Atualmente, estou estudando design e programação web, adentrando no universo da criação online.",
+    en: "Hello! 👋 My name is Izaelle Lavigne, and I am a dedicated designer passionate about crafting digital experiences.",
+    pt: "Olá! 👋 Meu nome é Izaelle Lavigne, e sou uma designer dedicada apaixonada por criar experiências digitais.",
   });
 
   const text2_1 = useT({
-    en: "Since I began my journey, I've had the opportunity to enhance my skills in responsive",
-    pt: "Desde que comecei minha jornada, tive a oportunidade de aprimorar minhas habilidades em design responsivo",
+    en: "I started my journey in technology early, graduating as a computer technician and continuing with front-end development.",
+    pt: "Iniciei minha jornada na tecnologia cedo, formando-me como técnica em informática e continuando com desenvolvimento front-end.",
   });
   const text2_bold = useT({
-    en: "design, UX/UI, and front-end developer",
-    pt: "design, UX/UI e desenvolvedora front-end",
+    en: "",
+    pt: "",
   });
   const text2_2 = useT({
-    en: "I have a keen eye for detail and a passion for creating visually appealing solutions that also provide an exceptional user experience.",
-    pt: "Tenho um olhar atento aos detalhes e uma paixão por criar soluções visualmente atraentes que também proporcionem uma experiência de usuário excepcional.",
+    en: "I discovered web design during this time and found my calling. Today, I focus on honing my skills in UX/UI design.",
+    pt: " Descobri o design web durante esse período e encontrei minha vocação. Hoje, foco em aprimorar minhas habilidades em design UX/UI.",
   });
+
+  useEffect(() => {
+    const particlesContainer = document.querySelector(".particles");
+
+    function createParticle() {
+      const particle = document.createElement("div");
+      particle.className = "particle";
+      const size = Math.random() * 4 + 1; // Random size between 1px to 5px
+      particle.style.width = `${size}px`;
+      particle.style.height = `${size}px`;
+      const position = Math.random() * 100 + "%"; // Random horizontal position
+      particle.style.left = position;
+      particlesContainer.appendChild(particle);
+
+      setTimeout(() => {
+        particle.remove(); // Remove particle after animation ends
+      }, 1500); // Adjust timing to match animation duration
+    }
+
+    setInterval(createParticle, 500);
+  }, []);
 
   return (
     <section className="px-[12px] md:grid md:grid-cols-2" id="about">
@@ -33,16 +55,19 @@ export const AboutSection = () => {
         <div className="mb-8 grid grid-cols-1 gap-6 text-[24px] leading-[36px]">
           <p>{text1}</p>
           <p>
-            {text2_1} <span className="font-semibold">{text2_bold}</span>.
+            {text2_1} <span className="font-semibold">{text2_bold}</span>
           </p>
           <p>{text2_2}</p>
         </div>
       </div>
-      <img
-        className="w-full object-cover object-top hidden md:block rounded-full"
-        src="images/avatar.png"
-        alt="Elle Avatar"
-      />
+      <div className="avatar-container hidden md:inline-block">
+        <img
+          className="w-full object-cover object-top  rounded-full "
+          src="images/avatar.png"
+          alt="Elle Avatar"
+        />
+        <div className="particles"></div>
+      </div>
     </section>
   );
 };
